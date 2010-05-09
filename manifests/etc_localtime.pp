7df1379d7d7f76139d77f16037e61e318c226b34
mh <mh@immerda.ch>
2010-05-09 23:59:05 +0200

    refactoring

--- manifests/etc_localtime.pp
+++ manifests/etc_localtime.pp
@@ -1,17 +1,6 @@
-# modules/localtime/manifests/init.pp - manage localtime stuff
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
-
-# modules_dir { "localtime": }
-
-class localtime {
-}
-
 define localtime::etc_localtime (
     $zoneinfo =  '/usr/share/zoneinfo/Europe/Zurich'
 ){
-    include localtime
-
     file { "/etc/localtime":
             ensure => link,
             target => "${zoneinfo}",
